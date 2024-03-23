b7c8745b7bfb6040fd49ffff404fad476d51d2c6
Bayu <60004072+Arcticoz@users.noreply.github.com>
2024-03-23 13:32:43 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-d9d1f73a-f374-4c32-b80c-9a686dc54e31/)
 
 
 echo "$curl_output" >> Update.tsx
